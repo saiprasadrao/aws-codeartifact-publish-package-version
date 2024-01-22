5673112cde3a5a2ddbfbc74f991b76f8d017fb86
Sai Prasad <prasad.sai.rao@outlook.com>
2024-01-22 23:22:41 +0530

    updated main.test.js

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -11,7 +11,6 @@ const getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
 const setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
 const setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
 
-
 // Mock the action's main function
 const runMock = jest.spyOn(main, 'run')
 const shaHashMock = jest.spyOn(shahash, 'creategzFile')
@@ -37,65 +36,6 @@ describe('action', () => {
     
     shaHashMock.mockRestore()
     runMock.mockRestore()
-    
-
-    // // Verify that all of the core library functions were called correctly
-    // expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
-    // expect(debugMock).toHaveBeenNthCalledWith(
-    //   2,
-    //   expect.stringMatching(timeRegex)
-    // )
-    // expect(debugMock).toHaveBeenNthCalledWith(
-    //   3,
-    //   expect.stringMatching(timeRegex)
-    // )
-    // expect(setOutputMock).toHaveBeenNthCalledWith(
-    //   1,
-    //   'time',
-    //   expect.stringMatching(timeRegex)
-    // )
-  })
   
-
-  // it('sets a failed status', async () => {
-  //   // Set the action's inputs as return values from core.getInput()
-  //   getInputMock.mockImplementation(name => {
-  //     switch (name) {
-  //       case 'milliseconds':
-  //         return 'this is not a number'
-  //       default:
-  //         return ''
-  //     }
-  //   })
-
-  //   await main.run()
-  //   expect(runMock).toHaveReturned()
-
-  //   // Verify that all of the core library functions were called correctly
-  //   expect(setFailedMock).toHaveBeenNthCalledWith(
-  //     1,
-  //     'milliseconds not a number'
-  //   )
-  // })
-
-  // it('fails if no input is provided', async () => {
-  //   // Set the action's inputs as return values from core.getInput()
-  //   getInputMock.mockImplementation(name => {
-  //     switch (name) {
-  //       case 'milliseconds':
-  //         throw new Error('Input required and not supplied: milliseconds')
-  //       default:
-  //         return ''
-  //     }
-  //   })
-
-  //   await main.run()
-  //   expect(runMock).toHaveReturned()
-
-  //   // Verify that all of the core library functions were called correctly
-  //   expect(setFailedMock).toHaveBeenNthCalledWith(
-  //     1,
-  //     'Input required and not supplied: milliseconds'
-  //   )
-  // })
+  })
 })
