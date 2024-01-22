8dc712067a60f6f44d9574381a38d2639dbe31fc
Sai Prasad <prasad.sai.rao@outlook.com>
2024-01-22 18:33:50 +0530

    reverted the changes

--- src/shahash.js
+++ src/shahash.js
@@ -3,33 +3,18 @@
  * @param {string} zipFileName Name of the tar.gz file to be created
  */
 const { createHash } = require('crypto')
-const tar = require('tar')
+const exec = require('@actions/exec')
 const fs = require('fs')
 
 async function creategzFile(folder, zipFileName = 'temp.tar.gz') {
-  // await exec.exec('tar', ['-czvf', zipFileName, folder])
-  let hash = ''
-  tar.create({
-    file: zipFileName,
-    cwd: folder,
-    gzip: true
-  },['.'])
-  .then(() => {
-    console.log('Tarball created successfully!')
-    fs.readFile(zipFileName, (err, data) => {
-      if (err) {
-        console.log(err, err.stack)
-      } else {
-        return createHash('sha256').update(data).digest('hex')
-      }
-    })
+  await exec.exec('tar', ['-czvf', zipFileName, folder])
+  fs.readFile(zipFileName, (err, data) => {
+    if (err) {
+      console.log(err, err.stack)
+    } else {
+      return createHash('sha256').update(data).digest('hex')
+    }
   })
-  .catch((error) => {
-    console.error('Error creating tarball:', error)
-  })
-  .finally(() => {
-    // Close the writable stream when done
-    tarStream.end()
-  })
+
 }
 module.exports = { creategzFile }
