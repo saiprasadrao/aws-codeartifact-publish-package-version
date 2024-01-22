061fd6448aecf5115563cee5fce90a7a9696eea5
Sai Prasad <prasad.sai.rao@outlook.com>
2024-01-22 18:46:01 +0530

    updated

--- __tests__/shahash.test.js
+++ __tests__/shahash.test.js
@@ -7,7 +7,7 @@ const { expect } = require('@jest/globals')
 
 describe('shahash', () => {
   it('create sha256 hash for a folder', async () => {
-    let hash = await creategzFile('src')
+    const hash = await creategzFile('src')
     console.log(hash)
     expect(hash).not.toBeFalsy()
   })
