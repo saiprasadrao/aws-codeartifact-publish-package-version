d3ee48458fa62704973f3a60dbe992973a2b3c35
Sai Prasad <prasad.sai.rao@outlook.com>
2024-01-22 14:59:05 +0530

    tab adjusted as per linter

--- src/createPackage.js
+++ src/createPackage.js
@@ -6,11 +6,10 @@ import { createHash } from 'crypto'
 const exec = require('@actions/exec')
 const fs = require('fs')
 
-
 async function creategzFile(folder, zipFileName = 'temp.tar.gz') {
-    await exec.exec('tar', ['-czvf', zipFileName, folder])
-    const buff = fs.readFileSync(zipFileName)
-    const hash = createHash('sha256').update(buff).digest('hex')
-    return hash
+  await exec.exec('tar', ['-czvf', zipFileName, folder])
+  const buff = fs.readFileSync(zipFileName)
+  const hash = createHash('sha256').update(buff).digest('hex')
+  return hash
 }  
 module.exports = { creategzFile }
