ad5f9b3a37679c22472c27ba65d334fce0f9b710
Sai Prasad <prasad.sai.rao@outlook.com>
2024-01-22 14:13:10 +0530

    addressed linter issues

--- src/createPackage.js
+++ src/createPackage.js
@@ -5,17 +5,15 @@
  * @param {string} zipFileName Name of the tar.gz file to be created.
  * 
  */
-const exec = require('@actions/exec');
-const fs = require("fs");
 import { createHash } from "crypto";
+const exec = require('@actions/exec');
+const fs = require('fs');
 
-let myOutput = '';
-let myError = '';
 
 async function creategzFile(folder, zipFileName=temp.tar.gz) {
-    await exec.exec('tar', ['-czvf', zipFileName, folder]);
-    const buff = fs.readFileSync(zipFileName);
-    const hash = createHash("sha256").update(buff).digest("hex");
-    return hash;
+    await exec.exec('tar', ['-czvf', zipFileName, folder])
+    const buff = fs.readFileSync(zipFileName)
+    const hash = createHash('sha256').update(buff).digest('hex')
+    return hash
 }  
 module.exports = { creategzFile }
