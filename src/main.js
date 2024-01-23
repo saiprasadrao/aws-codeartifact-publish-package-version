5bb074ab00b2430eb478cf4d4b02d710d7694e78
Sai Prasad <prasad.sai.rao@outlook.com>
2024-01-23 21:15:19 +0530

    updated main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core')
 const { creategzFile } = require('./shahash')
-const { CodeartifactClient, PublishPackageVersionCommand } = require('@aws-sdk/client-codeartifact')
+// const { CodeartifactClient, PublishPackageVersionCommand } = require('@aws-sdk/client-codeartifact')
+const exec = require('@actions/exec')
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
@@ -18,21 +19,43 @@ async function run() {
 
     const hash = await creategzFile(folder)
 
-    const params = {
-      assetContent: 'temp.tar.gz',
-      assetName: 'temp.tar.gz', // required
-      assetSHA256: hash, // required
+    // const params = {
+    //   assetContent: 'temp.tar.gz',
+    //   assetName: 'temp.tar.gz', // required
+    //   assetSHA256: hash, // required
+    //   domain,
+    //   format,
+    //   package: packageName, // required
+    //   packageVersion, // required
+    //   repository, // required
+    //   domainOwner,
+    //   namespace: packageName
+    // }
+    await exec.exec('aws', [
+      'codeartifact',
+      'publish-package-version',
+      '--domain',
       domain,
+      '--repository',
+      repository,
+      '--format',
       format,
-      package: packageName, // required
-      packageVersion, // required
-      repository, // required
-      domainOwner,
-      namespace: packageName
-    }
-    const client = new CodeartifactClient({})
-    const command = new PublishPackageVersionCommand(params)
-    const response = await client.send(command)
+      '--namespace',
+      packageName,
+      '--package',
+      packageName,
+      '--package-version',
+      packageVersion,
+      '--asset-content',
+      'temp.tar.gz',
+      '--asset-name',
+      'temp.tar.gz',
+      '--asset-sha256',
+      hash
+    ])
+    // const client = new CodeartifactClient({})
+    // const command = new PublishPackageVersionCommand(params)
+    // const response = await client.send(command)
     core.setCommandEcho(true)
   } catch (error) {
     // Fail the workflow run if an error occurs
