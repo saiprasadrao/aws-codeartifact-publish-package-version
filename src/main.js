b1ea5cd9f58c963541a7ead2e32adb26eb47d105
Sai Prasad <prasad.sai.rao@outlook.com>
2024-01-23 22:01:39 +0530

    command updated to client.publish

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core')
 const { creategzFile } = require('./shahash')
-const { CodeartifactClient, PublishPackageVersionCommand } = require('@aws-sdk/client-codeartifact')
+const { Codeartifact } = require('@aws-sdk/client-codeartifact')
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
@@ -31,10 +31,10 @@ async function run() {
       namespace: packageName
     }
 
-    const client = new CodeartifactClient({})
+    const client = new Codeartifact({ region: awsRegion })
     // const command = new PublishPackageVersionCommand(params)
     // const response = await client.send(command)
-    const response = await client.PublishPackageVersionCommand(params)
+    const response = await client.publishPackageVersion(params)
     core.setCommandEcho(true)
   } catch (error) {
     // Fail the workflow run if an error occurs
