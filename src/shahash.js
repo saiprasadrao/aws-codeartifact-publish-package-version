412b50b1708dd54bbb68409ce6ae4f6d88af3e17
Sai Prasad <prasad.sai.rao@outlook.com>
2024-01-31 21:47:17 +0530

    updated hash function

--- src/shahash.js
+++ src/shahash.js
@@ -5,13 +5,15 @@
 const exec = require('@actions/exec')
 const fs = require('fs')
 const { Sha256 } = require('@aws-crypto/sha256-js')
+const { Buffer } = require('buffer')
 
 async function creategzFile(folder, zipFileName = 'temp.tar.gz') {
   await exec.exec('tar', ['-czvf', zipFileName, folder])
   const data = fs.readFileSync(zipFileName)
   const hash = new Sha256()
   hash.update(data)
-  const shaHash = await hash.digest()
+  const digest = await hash.digest()
+  const shaHash = Buffer.from(digest).toString('hex')
   return shaHash
 }
 
